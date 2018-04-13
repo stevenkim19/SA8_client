e3f5b7a1cc2a69d24342b2cbec1bcf44ba72bfd5
Steven Kim <steven.kim.19@dartmouth.edu>
2018-04-13 18:19:14 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+const time = {
+  count: 0,
+  pass() {
+    this.count += 1;
+    return this.count;
+  },
+};
+
+setInterval(() => {
+  const message = `You have been here for ${time.count} seconds`;
+  time.pass();
+  console.log(time.count);
+  $('#main').html(message);
+}, 1000);
