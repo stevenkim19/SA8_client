3b4c87b2747644bfe9f8a7a11a587cfa57d7dd82
Steven Kim <steven.kim.19@dartmouth.edu>
2018-04-19 16:33:25 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import './style.scss';
 
-const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: Immutable.Map(),
+    };
+  }
+  render() {
+    return (
+      <div>
+        <p> hello!</p>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
-
-
-$('#main').html('Here we go!');
-
-const time = {
-  count: 0,
-  pass() {
-    this.count += 1;
-    return this.count;
-  },
-};
-
-setInterval(() => {
-  const message = `You have been here for ${time.count} seconds`;
-  time.pass();
-  console.log(time.count);
-  $('#main').html(message);
-}, 1000);
